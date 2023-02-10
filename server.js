cf192082880ed739747134dea9d39393ffb31c83
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2023-02-10 15:28:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample! My name is Alina Khan. I am creating Node JS Application\n');
+  res.send('Hello Node.js! My name is Alina Khan. I am creating Node JS Application.\n');
 });
 
 var port = process.env.PORT||PORT;
